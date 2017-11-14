a2eed9deb13d752052a060ba3bdc2073d77cd9c5
Lucas <lucazzzv89@gmail.com>
2017-11-14 09:36:33 -0500

    app init setup

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => {
+    res.type('html');
+    res.write('<h2>Test app with postgreSQL and Express</h2>');
+    res.end();
+});
+
+module.exports = app;
